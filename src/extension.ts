9667228b7de8124953488f126d3670de7a803e92
t.t <7117978+ttodua@users.noreply.github.com>
2024-09-05 10:54:40 +0400

    add options

--- src/extension.ts
+++ src/extension.ts
@@ -4,20 +4,16 @@ import * as vscode from 'vscode';
 
 const childProcess = require("child_process");
 
-// this method is called when your extension is activated
+
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "TortoiseGit Buttons" is now active!');
-
+    registerSettings();
 	const allCommands = [
 		'sync', 'pull', 'fetch', 'commit', 'push', 'stashsave', 'stashapply', 'stashpop', 'rename', 'revert',  'log', 'blame', 'diff', 'repostatus', 'showcompare', 'refbrowse', 'reflog', 'repobrowser', 'revisiongraph', 'resolve', 'conflicteditor', 'cleanup', 'rebase', 'merge', 'switch', 'add', 'remove', 'ignore', 'bisect', 'tag', 'settings', 'subadd', 'subupdate', 'subsync', 'export', 'lfslocks', 'daemon'
 	];
 	for (const value of allCommands) {
 		let disposable = vscode.commands.registerCommand("puvox.tgit." + value, (uri) => {
-			let path = getPath(uri);
+			let path = getPathOfChosenFile(uri);
 			console.log("TortoiseGit: " + value + " -> " + path);
 			execTgCommand(value, path);
             //vscode.window.showInformationMessage('')
@@ -27,21 +23,31 @@ export function activate(context: vscode.ExtensionContext) {
 }
 
 
-function execTgCommand(cmd : string, path : string) {
-    childProcess.exec(`TortoiseGitProc.exe /command:${cmd} /path:"${path}"`, (error : any, stdout : any, stderr : any) => {
-        if (error) {
-            console.warn(`TortoiseGit [error] -> ${error}`);
-        }
-        if (stdout) {
-            console.log(`TortoiseGit [stdout] -> ${stdout}`);
-        }
-        if (stderr) {
-            console.error(`TortoiseGit [stderr] -> ${stderr}`);
-        }
-    });
+async function execTgCommand(cmd : string, path : string) {
+    const location = vscode.workspace.getConfiguration('TortoisegitButtons').get('TortoiseLocation') as string;
+    const pathToExe = location + '/bin/TortoiseGitProc.exe';
+    try {
+        await vscode.workspace.fs.stat(vscode.Uri.file(pathToExe));
+        childProcess.exec(`"${pathToExe}" /command:${cmd} /path:"${path}"`, (error : any, stdout : any, stderr : any) => {
+            if (error) {
+                console.warn(`TortoiseGit [error] -> ${error}`);
+            }
+            if (stdout) {
+                console.log(`TortoiseGit [stdout] -> ${stdout}`);
+            }
+            if (stderr) {
+                console.error(`TortoiseGit [stderr] -> ${stderr}`);
+            }
+        });
+    } catch (error) {
+        const selection = await vscode.window.showWarningMessage('Can not find TortoiseGit installation. If you have installed it, enter the path here', 'Open settings');
+		if (selection !== undefined) {
+            openSettings();
+		}
+    }
 }
 
-function getPath(uri : any) {
+function getPathOfChosenFile(uri : any) {
     if (uri && uri.fsPath) {
         return uri.fsPath;
     }
@@ -51,6 +57,14 @@ function getPath(uri : any) {
     else {
         return vscode.workspace.rootPath;
     }
+}
+
+function registerSettings() {
+    vscode.workspace.getConfiguration('TortoisegitButtons').update('TortoiseLocation', 'C:\\Program Files\\TortoiseGit', vscode.ConfigurationTarget.Workspace);
+}
+
+function openSettings() {
+    vscode.commands.executeCommand('workbench.action.openSettings', 'TortoisegitButtons.TortoiseLocation');
 }
 
 // this method is called when your extension is deactivated
